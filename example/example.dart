2b8ae2746f9d7250d67b150647474242bbd97beb
Farhan Mac <farhan.fadila1717@gmail.com>
2022-05-28 12:09:51 +0700

    update example

--- example/example.dart
+++ example/example.dart
@@ -1,66 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:scroll_snap_effect/scroll_snap_effect.dart';
 
-void main() {}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: const ExamplePage(),
-    );
-  }
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ExamplePage(),
+    ),
+  );
 }
 
 class ExamplePage extends StatelessWidget {
-  const ExamplePage({
-    Key? key,
-  }) : super(key: key);
+  const ExamplePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          height: 100,
-          width: double.infinity,
-          child: ScrollSnapEffect(
-            itemSize: 116,
-            itemCount: 15,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            onChanged: (index) {
-              debugPrint('$index');
-            },
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(right: 16),
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: ColoredBox(
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        '$index',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
+      appBar: AppBar(
+        title: const Text("Example"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text("scrollDirection: Axis.horizontal"),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 100,
+            width: double.infinity,
+            child: ScrollSnapEffect(
+              itemSize: 110,
+              itemCount: 10,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: SizedBox.square(
+                    dimension: 100,
+                    child: ColoredBox(
+                      color: Colors.lightBlue,
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+            child: Text("scrollDirection: Axis.vertical"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SizedBox(
+              height: 100,
+              width: 100,
+              child: ScrollSnapEffect(
+                itemSize: 110,
+                itemCount: 10,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: SizedBox.square(
+                      dimension: 100,
+                      child: ColoredBox(
+                        color: Colors.amber,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
